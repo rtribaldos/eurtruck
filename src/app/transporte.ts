8a6ed3225c7f930cc4b418558a981cdaef682fea
lydia <raultribaldos@gmail.com>
2017-10-08 23:20:49 +0200

    Evolution

---
+++ src/app/transporte.ts
@@ -0,0 +1,14 @@
+export interface ITransporte{
+
+    idUsuario : string ;
+    idTransporte : string ;
+    origen  : string ;
+    destino  : string  ;
+    carga  : string ;
+    fechaCarga : Date;
+    fechaDescarga : Date;
+    mercancia : Date;
+    observaciones : string;
+    especificaciones : string;
+    codigoLavado : string;
+}
--- src/pages/nuevoViaje/nuevoViaje.ts
+++ src/pages/nuevoViaje/nuevoViaje.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController } from 'ionic-angular';
 import { FirebaseListObservable, AngularFireDatabase  } from 'angularfire2/database';
 import { OfertasPage } from '../ofertas/ofertas';
+import { ITransporte } from '../../app/transporte';
  
 @Component({
   selector: 'page-nuevoViaje',
@@ -12,63 +13,41 @@ export class NuevoViajePage {
  
     @ViewChild('signupSlider') signupSlider: any;
     viajes: FirebaseListObservable<any>;  
-     
-    slideOneForm: FormGroup;
-    slideTwoForm: FormGroup;
- 
-    submitAttempt: boolean = false;
- 
-    constructor(public navCtrl: NavController, public formBuilder: FormBuilder, 
-        public database: AngularFireDatabase) {
-
-      this.viajes = this.database.list('/viajes');
-      
-      this.slideOneForm = formBuilder.group({
-          destino: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
-          carga:['',],
-          fechac:['',],
-          fechad:['',],
-          mercancia:['',]
-      });
- 
-      this.slideTwoForm = formBuilder.group({
-            origen: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
-            observaciones:['',],
-            especificaciones:['',],
-            codigoLavado:['',]
-      });
-
-      
-    }
- 
-    next(){
-        this.signupSlider.slideNext();
-    }
- 
-    prev(){
-        this.signupSlider.slidePrev();
-    }
- 
-  save(){
+    viajeNuevo: ITransporte;      
+    myForm: FormGroup;
     
-    this.viajes.push({
-        destino: this.slideOneForm.value.destino,
-        origen: this.slideTwoForm.value.origen,
-        carga: this.slideOneForm.value.carga,
-        fechac: this.slideOneForm.value.fechac,
-        fechad: this.slideOneForm.value.fechad,
-        mercancia: this.slideOneForm.value.mercancia,
-        observaciones: this.slideTwoForm.value.observaciones,
-        especificaciones: this.slideTwoForm.value.especificaciones,
-        codigoLavado: this.slideTwoForm.value.codigoLavado,
+ 
+    constructor(public navCtrl: NavController, public builder: FormBuilder, 
+        public database: AngularFireDatabase) {
+      this.viajes = this.database.list('/viajes');
+      this.myForm = builder.group({
+        'origen': ['',],
+        'destino': ['',],
+        'fechac':['',],
+        'fechad':['',],
+        'carga':['',],
+        'mercancia':['',],
+        'observaciones':['',],
+        'especificaciones':['',],
+        'codigoLavado':['',]
+      })
+    } 
+
+    onSubmit(formData) {
+      console.log('Form data is '  + formData.value.origen);
+      this.viajes.push({
+        destino: formData.value.destino,
+        origen: formData.value.origen,
+        carga: formData.value.carga,
+        fechac: formData.value.fechac,
+        fechad: formData.value.fechad,
+        mercancia: formData.value.mercancia,
+        observaciones: formData.value.observaciones,
+        especificaciones: formData.value.especificaciones,
+        codigoLavado: formData.value.codigoLavado,
         done: false
-    });
-
-
+     });
      this.navCtrl.popTo(OfertasPage);
+    }
  
-  }
- 
-  
-
 }
