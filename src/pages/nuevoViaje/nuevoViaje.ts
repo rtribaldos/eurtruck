d84e3ceb4772088876c86028354b8204c8d86e58
elfogre <eb.jose@gmail.com>
2017-12-10 01:51:26 +0100

    Se integra google maps con los nuevos viajes

--- src/pages/nuevoViaje/nuevoViaje.ts
+++ src/pages/nuevoViaje/nuevoViaje.ts
@@ -11,12 +11,29 @@ declare var google;
   templateUrl: 'nuevoViaje.html'
 })
 export class NuevoViajePage {
-
+  
     @ViewChild('map') mapElement: ElementRef;
     map: any;
     viajes: FirebaseListObservable<any>;  
     myForm: FormGroup;
     localUser: any; 
+    autocompleteOrigen: any;
+    autocompleteDestino: any;
+    poly = new google.maps.Polyline({
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 3,
+      map: this.map,
+    });
+    markerOrigen = new google.maps.Marker({
+      map: this.map,
+      label: 'O'
+    });
+  
+    markerDestino = new google.maps.Marker({
+      map: this.map,
+      label: 'D'
+    });
     
  
     constructor(public navCtrl: NavController, public builder: FormBuilder, 
@@ -57,16 +74,71 @@ export class NuevoViajePage {
 
     ionViewDidLoad(){
       this.loadMap();
+      var inputOrigen = document.getElementById('originText').getElementsByTagName('input')[0];
+      var inputDestino = document.getElementById('destinyText').getElementsByTagName('input')[0];
+      var options = {componentRestrictions: {country: 'es'}};
+      this.autocompleteOrigen = new google.maps.places.Autocomplete(inputOrigen, options);
+      this.autocompleteDestino = new google.maps.places.Autocomplete(inputDestino, options);
+      this.autocompleteOrigen.addListener('place_changed', () => {
+        var place = this.autocompleteOrigen.getPlace();
+        if (place.geometry) {
+          this.markerOrigen.setPosition(place.geometry.location);
+          this.markerOrigen.setMap(this.map);
+          if (this.autocompleteDestino.getPlace() && this.autocompleteDestino.getPlace().geometry) {
+            var bounds = new google.maps.LatLngBounds();
+            bounds.extend(place.geometry.location);
+            bounds.extend(this.autocompleteDestino.getPlace().geometry.location);
+            this.map.fitBounds(bounds);
+            var path = [place.geometry.location, this.autocompleteDestino.getPlace().geometry.location];
+            this.poly.setPath(path);
+            this.poly.setMap(this.map);
+          } else {
+            this.map.panTo(place.geometry.location);
+            this.map.setZoom(15);
+          }
+        } else {
+          document.getElementById('originText').getElementsByTagName('input')[0].placeholder = 'Enter a city';
+        }
+      });
+      this.autocompleteDestino.addListener('place_changed', () => {
+        var place = this.autocompleteDestino.getPlace();
+        if (place.geometry) {
+          this.markerDestino.setPosition(place.geometry.location);
+          this.markerDestino.setMap(this.map);
+          if (this.autocompleteOrigen.getPlace() && this.autocompleteOrigen.getPlace().geometry) {
+            var bounds = new google.maps.LatLngBounds();
+            bounds.extend(place.geometry.location);
+            bounds.extend(this.autocompleteOrigen.getPlace().geometry.location);
+            this.map.fitBounds(bounds);
+            var path = [place.geometry.location, this.autocompleteOrigen.getPlace().geometry.location];
+            this.poly.setPath(path);
+            this.poly.setMap(this.map);
+          } else {
+            this.map.panTo(place.geometry.location);
+            this.map.setZoom(15);
+          }
+        } else {
+          document.getElementById('destinyText').getElementsByTagName('input')[0].placeholder = 'Enter a city';
+        }
+      });
     }
+
+    ngAfterViewInit() {
+      
+  }
    
     loadMap(){
    
-      let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+      let latLng = new google.maps.LatLng(40.5, -3.7);
    
       let mapOptions = {
         center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        zoom: 5,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        panControl: false,
+        zoomControl: false,
+        streetViewControl: false
       }
    
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
